f2176955e802d235d8b576017d4cf6021be2b8e5
orgeisler <orgeisler1@gmail.com>
2022-09-07 17:52:32 +0300

    save users in the localstorage as a list

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,35 @@ let renderer: Renderer = new Renderer()
 
 $(".gen-butm").on('click', async function () {
     await dataModule.generatAll()
+    // console.log(dataModule)
     renderer.RenderAll(dataModule)
 })
 
 $(".save-butm").on('click',function () {
-    localStorage.setItem(`${dataModule.person.firstName}`, JSON.stringify(dataModule))
-    console.log(localStorage)
+    if (localStorage.length === 0) {
+        localStorage.setItem('userArry', JSON.stringify([dataModule]))
+        console.log(localStorage)
+    }
+    else{
+        const userArry = JSON.parse(localStorage.userArry)
+        // console.log(userArry)
+        userArry.push(dataModule)
+        localStorage.clear()
+        localStorage.setItem('userArry', JSON.stringify(userArry))
+        // console.log(localStorage)
+    }
 })
+
+$('.load-butm').on('click',function() {
+    if (localStorage.length>0) {
+        const localStorageObject = JSON.parse(localStorage.userArry)
+        const userLoaded = new DataModule()
+        userLoaded.person = localStorageObject[0].person
+        userLoaded.pokemon = localStorageObject[0].pokemon 
+        userLoaded.quote = localStorageObject[0].quote 
+        userLoaded.meatText = localStorageObject[0].meatText
+        console.log(userLoaded)
+        renderer.RenderAll(userLoaded)
+            }
+
+    })
